394d28912a8ba08364762950034fcea0190cce19
Facundo Pidal <facundopidalruizdiaz@gmail.com>
2024-10-27 18:26:19 -0300

    pagina sobre nossotros

--- src/app/pages/about-us/about-us.component.ts
+++ src/app/pages/about-us/about-us.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
-import { AppComponent } from "../../app.component";
 import { NavBarComponent } from '../../components/nav-bar/nav-bar.component';
 
 @Component({
   selector: 'app-about-us',
   standalone: true,
-  imports: [AppComponent, 
+  imports: [
     NavBarComponent],
   templateUrl: './about-us.component.html',
   styleUrl: './about-us.component.css'
